37e4c064fb0f088c0db10714ede93758e3a56deb
wiggoen <wiggoen@gmail.com>
2018-02-14 14:51:26 +0100

    moved include inside definition

--- Project_1/src/unit_test.cpp
+++ Project_1/src/unit_test.cpp
@@ -1,7 +1,6 @@
 //#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-#include "inc/catch.hpp"
 #ifdef CATCH_CONFIG_MAIN
-
+#include "inc/catch.hpp"
 
 
 
