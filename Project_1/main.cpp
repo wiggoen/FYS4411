fc47a92bf21c7d36e5b6502af01d1a4c8d6f4cff
wiggoen <wiggoen@gmail.com>
2018-02-14 14:53:37 +0100

    removed namespace to aviod conflicts

--- Project_1/main.cpp
+++ Project_1/main.cpp
@@ -1,11 +1,9 @@
 #ifndef CATCH_CONFIG_MAIN
 #include <iostream>
 
-using namespace std;
-
 int main()
 {
-    cout << "Hello World!" << endl;
+    std::cout << "Hello World!" << std::endl;
     return 0;
 }
 
