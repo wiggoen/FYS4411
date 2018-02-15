020228d33676d7867c602a4e7b5fbd7fc8ddf6b5
linegpe <line.gaard.pedersen@gmail.com>
2018-02-15 17:48:10 +0100

    Wavefunc now runs without errore

--- Project_1/src/wavefunction.cpp
+++ Project_1/src/wavefunction.cpp
@@ -1,5 +1,6 @@
 #include "inc/wavefunction.h"
 #include <math.h>
+#include <cmath>
 
 Wavefunction::Wavefunction(int nParticles, double** positionMatrix)
 {
@@ -7,24 +8,26 @@ Wavefunction::Wavefunction(int nParticles, double** positionMatrix)
     // * prod(i<j){f(a,|r_i-r_j|}
 
     double* psi = new double(100);
+    double** R = positionMatrix;
 
 
 
     double g; // = exp(x^2+y^2+z^2);
     double f;
 
+    double a = 1;
     int i = 0;
     double* r = new double(nParticles);
 
     for (int i=0; i<nParticles; i++)
     {
-        r[i] = sqrt(positionMatrix[i,0],positionMatrix[i,1],positionMatrix[i,3]);
+        r[i] = sqrt(R[i][0]*R[i][0]+R[i][1]*R[i][1]+R[i][2]*R[i][2]);
 
         g = exp(r[i]*r[i]);
         for (int j=0; j<nParticles; j++)
         {
-            r[i] = sqrt(positionMatrix[j,0],positionMatrix[j,1],positionMatrix[j,3]);
-                        if (abs(r[i]-r[j])>a) {f = 1-a/abs(r[i]-r[j]);}
+            r[j] = sqrt(R[j][0]*R[j][0]+R[j][1]*R[j][1]+R[j][2]*R[j][2]);
+                        if (std::abs(r[i]-r[j])>a) {f = 1-a/std::abs(r[i]-r[j]);}
                         else {f = 0;}
             }
         }
