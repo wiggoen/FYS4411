9ff78d41c71c8a2f001274f170d6f2810a64d0b9
linegpe <line.gaard.pedersen@gmail.com>
2018-02-19 10:47:54 +0100

    made f and g separate functions

--- Project_1/src/wavefunction.cpp
+++ Project_1/src/wavefunction.cpp
@@ -1,4 +1,5 @@
 #include "inc/wavefunction.h"
+#include "inc/matrix.h"
 #include <math.h>
 #include <cmath>
 
@@ -7,13 +8,14 @@ Wavefunction::Wavefunction(int nParticles, double** positionMatrix)
     //psi = prod(i){g(alpha,beta,r_i)}
     // * prod(i<j){f(a,|r_i-r_j|}
 
-    double* psi = new double(100);
+    Matrix matrix;
+
+    int N = 100;
+    double* psi = new double(N);
+
     double** R = positionMatrix;
-
-
-
-    double g; // = exp(x^2+y^2+z^2);
-    double f;
+    double* g = new double(N);
+    double** f = matrix.makeMatrix(nParticles, 2);
 
     double a = 1;
     int i = 0;
@@ -21,17 +23,32 @@ Wavefunction::Wavefunction(int nParticles, double** positionMatrix)
 
     for (int i=0; i<nParticles; i++)
     {
-        r[i] = sqrt(R[i][0]*R[i][0]+R[i][1]*R[i][1]+R[i][2]*R[i][2]);
-
-        g = exp(r[i]*r[i]);
         for (int j=0; j<nParticles; j++)
         {
-            r[j] = sqrt(R[j][0]*R[j][0]+R[j][1]*R[j][1]+R[j][2]*R[j][2]);
-                        if (std::abs(r[i]-r[j])>a) {f = 1-a/std::abs(r[i]-r[j]);}
-                        else {f = 0;}
-            }
+
         }
     }
+    //psi[i] = g*f;
+}
+
+double g(double* position)
+{
+    double* p = position;
+    double r = sqrt(p[0]*p[0]+p[1]*p[1]+p[2]*p[2]);
+    return exp(r*r);
+}
+
+double f(double* position1, double* position2, double a)
+{
+    double* p1 = position1;
+    double* p2 = position2;
+    double r1 = sqrt(p1[0]*p1[0]+p1[1]*p1[1]+p1[2]*p1[2]);
+    double r2 = sqrt(p2[0]*p2[0]+p2[1]*p2[1]+p2[2]*p2[2]);
+    if (std::abs(r1-r2) > a)
+    {
+        return 1-a/(std::abs(r1-r2));
+    } else {return 0;}
+}
 
 
 Wavefunction::~Wavefunction()
