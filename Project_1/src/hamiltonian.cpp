de1fdb6198ebb6633b531ff6afcb0ab260be7ba1
linegpe <line.gaard.pedersen@gmail.com>
2018-02-22 18:23:48 +0100

    fffffff

--- Project_1/src/hamiltonian.cpp
+++ Project_1/src/hamiltonian.cpp
@@ -10,19 +10,25 @@ Hamiltonian::Hamiltonian(int nParticles, const arma::vec &x)
     // H = sum(i-N){-hbar^2/2m del_i^2 + V_ext(r_i)}
     // + sum(i<j-N){V_int(r_i,r_j)}
 
-
-    wf = new Wavefunction(0,x);
-
     // For now this only calculates the spherical potential
     double sum1, sum2;
     double h = 0.0001;
     int N = nParticles;
     double alpha = 0.5;
     double V_ext = 0;
+    double V_int = 0;
     for (int i=0; i<N; i++)
     {
         sum1 += - alpha * DoubleDerivative(N,h,x) + V_ext;
+        for (int j=0; j<N; j++)
+        {
+            if(i<j)
+            {
+                sum2 += V_int;
+            }
+        }
     }
+    double sum = sum1+sum2;
 }
 
 double Hamiltonian::Derivative(int nParticles, double h, arma::vec x)
