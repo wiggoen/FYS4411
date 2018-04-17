13ebb1df4e5f1c6eab95150b625b108933cd304b
wiggoen <wiggoen@gmail.com>
2018-04-17 14:42:18 +0200

    script for running in python3

---
+++ Project_1/scripts/py3-run.py
@@ -0,0 +1,45 @@
+import subprocess
+#import os
+
+
+def run_cmd(cmd):
+    print(" ")
+    print(">> {}".format(cmd))
+    subprocess.call(cmd, shell=True)
+
+
+# File names
+fname1 = "../main"
+fname2 = "../src/hamiltonian"
+fname3 = "../src/tests"
+fname4 = "../src/variationalmontecarlo"
+fname5 = "../src/wavefunction"
+
+
+# Parameters
+nParticles = 6
+nDimensions = 3
+nCycles = 1e6
+alpha = 0.5
+stepLength = 0.1
+timeStep = 0.01
+cycleStepToFile = nCycles
+trials = 1
+# Choices: "bf" (Brute force) or "im" (Importance)
+samplingType = "im"
+# Choices: "ana" (Analytical) or "num" (Numerical) or "int" (Interaction)
+derivationType = "int"
+
+
+# Library used
+library = "-L/usr/local/Cellar/armadillo/8.400.0/lib/ -larmadillo"
+
+# Location of include files / header files
+includes = "-I/usr/local/Cellar/armadillo/8.400.0/include/ -I/Users/trondwj/GitHub/FYS4411/Project_1"
+
+
+# Compile
+run_cmd("g++ -std=c++11 -O3 {} {} -o {}.out {}.cpp {}.cpp {}.cpp {}.cpp {}.cpp".format(library, includes, fname1, fname1, fname2, fname3, fname4, fname5))
+
+# Run
+run_cmd("./{}.out {} {} {} {} {} {} {} {} {} {}".format(fname1, nParticles, nDimensions, nCycles, alpha, stepLength, timeStep, cycleStepToFile, trials, samplingType, derivationType))
