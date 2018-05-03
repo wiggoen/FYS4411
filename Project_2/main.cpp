40d03a080953dc877973593d4aa48a43be87edf7
wiggoen <wiggoen@gmail.com>
2018-05-03 14:53:46 +0200

    added more parameters

--- Project_2/main.cpp
+++ Project_2/main.cpp
@@ -9,23 +9,32 @@ using json = nlohmann::json; // for convenience
 int main()//int numberOfArguments, char *arguments[])
 {
     /* The whole project path is needed to read json on mac */
-    // Am I Trond Wiggo?
-    //std::string projectFolder = "/Users/trondwj/GitHub/FYS4411/Project_2/";
 
-    // Am I Line?
-    std::string projectFolder = "/home/line/github/FYS4411/Project_2/";
-    // --
+    /* Who am I? */
+    std::string Iam = "Line";
+    std::string projectFolder;
+    if (Iam == "Trond") {
+        std::cout << "Hi, Trond!" << std::endl;
+        projectFolder = "/Users/trondwj/GitHub/FYS4411/Project_2/";
+    } else if (Iam == "Line") {
+        std::cout << "Hi, Line!" << std::endl;
+        projectFolder = "/home/line/github/FYS4411/Project_2/";
+    } else {
+        std::cerr << "I don't know who you are.." << std::endl;
+        exit(1);
+    }
+
 
     /* READ PARAMETERS */
     std::ifstream inputFile(projectFolder+"parameters.json", std::ios::in);
     json parameter;
     inputFile >> parameter;
+    int nParticles = parameter["nParticles"];
+    int nDimensions = parameter["nDimensions"];
+    int nCycles = parameter["nCycles"];
     double alpha = parameter["alpha"];
-    std::cout << alpha << std::endl;
+    double beta = parameter["beta"];
 
 
-
-
-    std::cout << "Hello World!" << std::endl;
     return 0;
 }
