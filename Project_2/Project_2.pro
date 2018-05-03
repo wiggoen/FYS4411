87f82357d47df2da7432971d6c7c840d232ac561
wiggoen <wiggoen@gmail.com>
2018-05-03 14:34:44 +0200

    read parameters using json

--- Project_2/Project_2.pro
+++ Project_2/Project_2.pro
@@ -3,5 +3,17 @@ CONFIG += console c++11
 CONFIG -= app_bundle
 CONFIG -= qt
 
+
+# Remove default optimization flag used by QT
+QMAKE_CXXFLAGS_RELEASE -= -O2
+
+# Add optimization flags
+#QMAKE_CXXFLAGS_RELEASE *= -O0  # Run this when developing
+QMAKE_CXXFLAGS_RELEASE *= -O3  # Run this on release
+
+
 SOURCES += \
         main.cpp
+
+HEADERS += \
+    inc/json.hpp
--- Project_2/main.cpp
+++ Project_2/main.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include "inc/json.hpp"
+
+using json = nlohmann::json; // for convenience
 
 
-int main()
+int main()//int numberOfArguments, char *arguments[])
 {
+    /* The whole project path is needed to read json on mac */
+    std::string projectFolder = "/Users/trondwj/GitHub/FYS4411/Project_2/";
+    // --
+
+    /* READ PARAMETERS */
+    std::ifstream inputFile(projectFolder+"parameters.json", std::ios::in);
+    json parameter;
+    inputFile >> parameter;
+    double alpha = parameter["alpha"];
+    std::cout << alpha << std::endl;
+
+
+
+
     std::cout << "Hello World!" << std::endl;
     return 0;
 }
