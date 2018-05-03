7fa32440fdc50654312f70b444754989b850e8a0
wiggoen <wiggoen@gmail.com>
2018-05-03 17:37:16 +0200

    added brute force.

--- Project_2/inc/hamiltonian.h
+++ Project_2/inc/hamiltonian.h
@@ -8,6 +8,7 @@ class Hamiltonian
 public:
     Hamiltonian( void );
     ~Hamiltonian( void );
+    static double LocalEnergy(const arma::mat &r, const int &nParticles, const double &alpha, const double &beta, const double &omega, const double &a);
 private:
 };
 
